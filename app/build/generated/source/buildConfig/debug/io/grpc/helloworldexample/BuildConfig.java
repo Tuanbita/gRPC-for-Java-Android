6c372efdca49088b20970541b73f29a78a0cf715
tuanbita <tuanbita@gmail.com>
2018-03-18 19:27:34 +0700

    abc

---
+++ app/build/generated/source/buildConfig/debug/io/grpc/helloworldexample/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.grpc.helloworldexample;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "io.grpc.android.helloworldexample";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
